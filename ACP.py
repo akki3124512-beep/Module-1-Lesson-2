c578538003d0c08efd03a17dc282117029cd22d2
akki3124512-beep <akki3124512@gmail.com>
2026-09-07 17:51:12 +0100

    Add files via upload

---
+++ ACP.py
@@ -0,0 +1,11 @@
+ak = "03/10/2012"
+m = "25/5/1984"
+d = "28/5/1982"
+g = "15/3/2014"
+am= "29/8/2014"
+
+print("My birthday is on", ak)
+print("My mum's birthday is on", m)
+print("My dad's birthday is on", d)
+print("My friend's birthday is on", g)
+print("My other friend's birthday is on", am)
